f0ab80994a7c40e349a6c83509b1bdd127a4bf1b
agiannell <agiannell@gmail.com>
2021-01-07 23:55:42 -0700

    completed code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+import './reset.css';
 import './App.css';
+import logo from './img/navbar-logo.svg'
+import menu from './img/menu-lines.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dropdownView: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+  
+  render() {
+    const {dropdownView} = this.state
+    const {toggleDropdown} = this;
+    return (
+      <section className="App">
+        <header>
+          <div className='header-container'>
+            <img src={logo} alt='nav logo' />
+            <nav className='desktop-menu'>
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+            <div className='menu-button' onClick={toggleDropdown}>MENU
+              <img src={menu} alt='hamburger menu' />
+            </div>
+          </div>
+          {dropdownView
+            ? (
+              <div className='dropdown'>
+                <div className='mobile-menu'>
+                  <span>SERVICES</span>
+                  <span>PORTFOLIO</span>
+                  <span>ABOUT</span>
+                  <span>TEAM</span>
+                  <span>CONTACT</span>
+                </div>
+              </div>
+            )
+            : null}
+        </header>
+        <section className='section-1'>
+          <div className='sec-1-container'>
+            <h1>Welcome To Our Studio!</h1>
+            <h2>IT'S NICE TO MEET YOU</h2>
+            <button>TELL ME MORE</button>
+          </div>
+        </section>
+      </section>
+    )
+  }
 }
 
 export default App;
